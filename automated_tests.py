e7f563398b7ce7e212ec19cba6e9a8751e90cbb2
William Silversmith <william.silversmith@gmail.com>
2024-01-27 23:49:58 -0500

    test: check range of rotation is sensible

--- automated_tests.py
+++ automated_tests.py
@@ -25,3 +25,18 @@ def test_single_voxel():
 	tri = np.sqrt(3) / 2 * ((0.5) ** 2)
 	hexagon = 6 * tri
 	assert np.isclose(area, hexagon)
+
+	# outside the voxel
+	area = xs3d.cross_sectional_area(voxel, [1,0,0], [1,0,0])
+	assert area == 0
+	area = xs3d.cross_sectional_area(voxel, [-1,0,0], [1,0,0])
+	assert area == 0
+
+	# arbitrary angles
+	for incr in range(11):
+		area = xs3d.cross_sectional_area(voxel, [0,0,0], [incr * 0.1,0,1])
+		assert area >= 1
+		assert area <= np.sqrt(2)
+
+
+
