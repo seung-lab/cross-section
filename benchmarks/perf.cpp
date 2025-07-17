99ea07bf1e0d5a2f057aa5767ef1ab6c2f41e043
William Silversmith <william.silversmith@gmail.com>
2025-07-17 17:24:21 -0400

    feat: add a simple performance benchmark and record results by version

---
+++ benchmarks/perf.cpp
@@ -0,0 +1,32 @@
+#include "src/xs3d.hpp"
+
+// g++ -std=c++17 -O2 -g test.cpp -o test
+
+void perf() {
+	const int sx = 500;
+	const int sy = 500;
+	const int sz = 500;
+
+	const int sxy = sx * sy;
+	const int voxels = sx * sy * sz;
+
+	uint8_t* binimg = new uint8_t[voxels]();
+	std::fill(binimg, binimg + voxels, 1);
+
+	for (int i = 0; i < sx; i++) {
+		float area = xs3d::cross_sectional_area(
+			binimg,
+			sx, sy, sz,
+			/*px=*/i, /*py=*/sy/2, /*pz=*/sz/2,
+			/*nx=*/1, /*ny=*/1, /*nz=*/1,
+			/*wx=*/1, /*wy=*/1, /*wz=*/1
+		);
+	}
+	
+	delete[] binimg;
+}
+
+int main() {
+	perf();
+	return 0;
+}
---
+++ benchmarks/versions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+XS_DIR=$HOME/code/cross-section
+BENCHMARKS_DIR="$XS_DIR/benchmarks"
+BIN_DIR=$BENCHMARKS_DIR/bin
+LOGFILE=$BENCHMARKS_DIR/version_timings.log
+
+mkdir $BIN_DIR
+rm $BIN_DIR/*
+rm $LOGFILE
+touch $LOGFILE
+
+for tag in $(git tag | sort); do
+	echo "Compiling $tag"
+	git checkout $tag
+	binary="$BIN_DIR/perf_$tag"
+	g++ -std=c++17 -O3 -I$XS_DIR -Wno-everything $BENCHMARKS_DIR/perf.cpp -o $binary
+
+	runtime=$( /usr/bin/time -p "$binary" 2>&1 >/dev/null | grep real | awk '{print $2}' )
+	echo "$tag: $runtime" >> $LOGFILE
+done;
+
+git checkout main
+
+echo "done."
